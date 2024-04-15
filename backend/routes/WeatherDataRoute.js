ef2df7019e8e25cdbac40e581059f4154578a4b2
Dominik Lewczyński <dominiklew200060@gmail.com>
2024-04-15 13:31:08 +0200

    change url structures in routes

--- backend/routes/WeatherDataRoute.js
+++ backend/routes/WeatherDataRoute.js
@@ -10,9 +10,9 @@ import {
 const router = express.Router();
 
 router.get('/weatherdata/all', getAllweatherdata);
-router.get('/weatherdata/:id', getWeatherdataById);
+router.get('/weatherdata/one/:id', getWeatherdataById);
 router.get('/weatherdata/filter', getFilteredWeatherdata);
-router.get("/expressflask/:cityName", ForecastWeather);
+router.get("/weatherdata/forecast/:cityName", ForecastWeather);
 router.get("/weatherdata", saveWeatherDatatoDB);
 
 export default router;
