1387f52b4ed05e383e3a15854321f2e91f1320e1
Dominik Lewczyński <dominiklew200060@gmail.com>
2024-04-16 16:50:45 +0200

    Change time in cron schedule

--- backend/index.js
+++ backend/index.js
@@ -47,7 +47,7 @@ app.use(WeatherDataRoute);
 
 // store.sync();
 
-cron.schedule('55 12 * * *', async () => {
+cron.schedule('0 14 * * *', async () => {
     try {
         await saveTodayWeatherData();
         console.log('Pobrano i zapisano dane z API forecast.');
