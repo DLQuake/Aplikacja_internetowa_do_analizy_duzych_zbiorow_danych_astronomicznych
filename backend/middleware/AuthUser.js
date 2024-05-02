5dcc2e7d80e4feb7c420468cbec57935b9ddd9f8
Dominik Lewczyński <dominiklew200060@gmail.com>
2024-05-02 19:20:24 +0200

    Translation of texts from Polish into English

--- backend/middleware/AuthUser.js
+++ backend/middleware/AuthUser.js
@@ -2,14 +2,14 @@ import User from "../models/UserModel.js";
 
 export const verifyUser = async (req, res, next) => {
     if (!req.session.userId) {
-        return res.status(401).json({ msg: "Zaloguj się do konta" });
+        return res.status(401).json({ msg: "Log in to your account" });
     }
     const user = await User.findOne({
         where: {
             uuid: req.session.userId
         }
     });
-    if (!user) return res.status(404).json({ msg: "Użytkownik nie znaleziony" });
+    if (!user) return res.status(404).json({ msg: "User not found" });
     req.userId = user.id;
     req.role = user.role;
     next();
@@ -21,7 +21,7 @@ export const adminOnly = async (req, res, next) => {
             uuid: req.session.userId
         }
     });
-    if (!user) return res.status(404).json({ msg: "Użytkownik nie znaleziony" });
-    if (user.role !== "admin") return res.status(403).json({ msg: "Brak dostępu" });
+    if (!user) return res.status(404).json({ msg: "User not found" });
+    if (user.role !== "admin") return res.status(403).json({ msg: "No access" });
     next();
 }
