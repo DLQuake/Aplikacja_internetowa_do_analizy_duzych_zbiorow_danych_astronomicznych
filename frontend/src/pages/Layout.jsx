1ab560d6550819828f8e36d94ad54314dc9419a4
Dominik Lewczyński <dominiklew200060@gmail.com>
2024-05-03 17:14:08 +0200

    Add border right in sidebar

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
 		<React.Fragment>
 			<Navbar />
 			<div className="columns mt-6" style={{ minHeight: "100vh" }}>
-				<div className="column is-2">
+				<div className="column is-2" style={{ borderRight: "2px solid #1F2229" }}>
 					<Sidebar />
 				</div>
 				<div className="column">
