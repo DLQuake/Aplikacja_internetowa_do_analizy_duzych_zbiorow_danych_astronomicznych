e4a6c3f19312588fdb1b06282a67681311b08dcd
Dominik Lewczyński <dominiklew200060@gmail.com>
2024-05-08 18:08:33 +0200

    Update routes and add route to delete report

--- backend/routes/ReportRoute.js
+++ backend/routes/ReportRoute.js
@@ -1,12 +1,14 @@
 import express from "express";
 import {
+    deleteReport,
     getAllreport,
     getReportById
 } from "../controllers/Report.js";
 
 const router = express.Router();
 
-router.get('/report/all', getAllreport);
-router.get('/report/one', getReportById);
+router.get('/report', getAllreport);
+router.get('/report/:id', getReportById);
+router.delete("/report/:id", deleteReport);
 
 export default router;
