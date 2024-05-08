3b774e3d863717950be14647c9dbbad2d2c6f464
Dominik Lewczyński <dominiklew200060@gmail.com>
2024-05-08 18:08:16 +0200

    Add route to predict forecast weather

--- backend/routes/ForecastRoute.js
+++ backend/routes/ForecastRoute.js
@@ -1,10 +1,13 @@
 import express from "express";
 import {
+    ForecastWeather,
     getAllForecast,
 } from "../controllers/Forecast.js";
+import { verifyUser } from "../middleware/AuthUser.js";
 
 const router = express.Router();
 
 router.get('/forecast/all', getAllForecast);
+router.get('/forecast/predict', verifyUser, ForecastWeather);
 
 export default router;
