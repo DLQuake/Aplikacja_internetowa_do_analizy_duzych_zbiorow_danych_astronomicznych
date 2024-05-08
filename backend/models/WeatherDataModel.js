adc63a70f3974d8da29eaccf5e801daf5a9b88b7
Dominik Lewczyński <dominiklew200060@gmail.com>
2024-05-08 14:18:29 +0200

    Delete Comment

--- backend/models/WeatherDataModel.js
+++ backend/models/WeatherDataModel.js
@@ -4,7 +4,6 @@ import Location from "./LocationModel.js";
 
 const { DataTypes } = Sequelize;
 
-// Model dla danych pogodowych
 const WeatherData = db.define('weatherdata', {
     uuid: {
         type: DataTypes.STRING,
