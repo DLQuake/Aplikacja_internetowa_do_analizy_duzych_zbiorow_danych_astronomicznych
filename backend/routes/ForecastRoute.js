4eb2058c27e225e7c0355564f0eb32b442fa5824
Dominik Lewczyński <dominiklew200060@gmail.com>
2024-05-10 12:12:45 +0200

    Add route to controller to get forecast by id of report

--- backend/routes/ForecastRoute.js
+++ backend/routes/ForecastRoute.js
@@ -2,12 +2,14 @@ import express from "express";
 import {
     ForecastWeather,
     getAllForecast,
+    getForecastsByReportAndLocation,
 } from "../controllers/Forecast.js";
 import { verifyUser } from "../middleware/AuthUser.js";
 
 const router = express.Router();
 
-router.get('/forecast/all', getAllForecast);
+router.get('/forecast/all', verifyUser, getAllForecast);
+router.get('/forecast/filter', verifyUser, getForecastsByReportAndLocation);
 router.get('/forecast/predict', verifyUser, ForecastWeather);
 
 export default router;
