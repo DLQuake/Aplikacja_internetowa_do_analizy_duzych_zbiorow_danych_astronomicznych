fb2ad9049968383bb61657222a86560082f330ff
Dominik Lewczyński <dominiklew200060@gmail.com>
2024-05-10 12:13:15 +0200

    Update component ForecastReports to see list of reports

--- frontend/src/components/ForecastReports.jsx
+++ frontend/src/components/ForecastReports.jsx
@@ -1,35 +1,22 @@
 import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import moment from 'moment';
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import { getWindDirection } from "../features/WindDirectionUtils";
 
 const ForecastReports = () => {
-    const [weatherdatas, setWeatherdatas] = useState([]);
-    const [cities, setCities] = useState([]);
-    const [selectedCity, setSelectedCity] = useState("");
-    const [startDate, setStartDate] = useState(null);
-    const [endDate, setEndDate] = useState(null);
+    const [reports, setReports] = useState([]);
     const [loading, setLoading] = useState(false);
     const tableRef = useRef(null);
 
     useEffect(() => {
-        getWeatherdata();
+        getReports();
     }, []);
 
-    const getWeatherdata = async () => {
+    const getReports = async () => {
         try {
             setLoading(true);
-            const response = await axios.get("http://localhost:5000/weatherdata/all");
-            setWeatherdatas(response.data.sort((a, b) => {
-                if (a.location.city === b.location.city) {
-                    return new Date(a.date) - new Date(b.date);
-                }
-                return a.location.city > b.location.city ? 1 : -1;
-            }));
-            const uniqueCities = [...new Set(response.data.map(weatherdata => weatherdata.location.city))];
-            setCities(uniqueCities);
+            const response = await axios.get("http://localhost:5000/report/");
+            setReports(response.data);
         } catch (error) {
             console.error("An error occurred while downloading data:", error);
         } finally {
@@ -37,39 +24,15 @@ const ForecastReports = () => {
         }
     };
 
-    const searchWeatherData = async () => {
-        setLoading(true);
+    const deleteReport = async (reportId) => {
         try {
-            const response = await axios.get("http://localhost:5000/weatherdata/filter", {
-                params: {
-                    city: selectedCity,
-                    startDate: startDate ? moment(startDate).format('YYYY-MM-DD') : null,
-                    endDate: endDate ? moment(endDate).format('YYYY-MM-DD') : null
-                }
-            });
-            setWeatherdatas(response.data.sort((a, b) => {
-                if (a.location.city === b.location.city) {
-                    return new Date(a.date) - new Date(b.date);
-                }
-                return a.location.city > b.location.city ? 1 : -1;
-            }));
-            tableRef.current.scrollIntoView({ behavior: "smooth" });
+            setLoading(true);
+            await axios.delete(`http://localhost:5000/report/${reportId}`);
+            await getReports();
+            alert("The report has been deleted.");
         } catch (error) {
-            console.error("An error occurred while downloading data:", error);
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    const refreshData = async () => {
-        try {
-            setLoading(true); // Ustawienie stanu loadingu na true
-            await axios.get("http://localhost:5000/weatherdata");
-            await getWeatherdata();
-            alert("The data has been updated.");
-        } catch (error) {
-            console.error("An error occurred while refreshing data:", error);
-            alert("An error occurred while refreshing data.");
+            console.error("An error occurred while deleting the report:", error);
+            alert("An error occurred while deleting the report.");
         } finally {
             setLoading(false);
         }
@@ -77,79 +40,40 @@ const ForecastReports = () => {
 
     return (
         <div className="pr-3">
-            <h1 className="title">Forecast Weather Reports</h1>
-            <div className="field">
-                <label className="label">Select City:</label>
-                <div className="control">
-                    <div className="select is-striped is-fullwidth">
-                        <select value={selectedCity} onChange={(e) => setSelectedCity(e.target.value)}>
-                            <option value="">Select city</option>
-                            {cities.map((city, index) => (
-                                <option key={index} value={city}>{city}</option>
-                            ))}
-                        </select>
-                    </div>
-                </div>
-            </div>
-            <label className="label">Select Date Range:</label>
-            <div className="field">
-                <div className="control">
-                    <DatePicker
-                        selectsRange={true}
-                        startDate={startDate}
-                        endDate={endDate}
-                        placeholderText="From - To"
-                        onChange={(update) => {
-                            setStartDate(update[0]);
-                            setEndDate(update[1]);
-                        }}
-                        isClearable={true}
-                        withPortal
-                        className="input"
-                    />
-                </div>
-            </div>
-            <div className="field">
-                <div className="control">
-                    <button className="button is-link" onClick={searchWeatherData}>Apply Filters</button>
-                </div>
-            </div>
+            <h1 className="title">Forecast Reports</h1>
             <div className="field">
                 <div className="control">
                     {loading ? (
                         <p className="title has-text-centered">Loading...</p>
                     ) : (
                         <div>
-                            <button className="button is-primary" onClick={refreshData}>Refresh Data</button>
                             <table className="table is-striped is-fullwidth" ref={tableRef}>
                                 <thead>
                                     <tr>
                                         <th>ID</th>
-                                        <th>City</th>
+                                        <th>Title</th>
                                         <th>Date</th>
-                                        <th>Temperature (°C)</th>
-                                        <th>Humidity (%)</th>
-                                        <th>Precipitation (mm)</th>
-                                        <th>Wind Speed (Km/h)</th>
-                                        <th>Wind Direction</th>
+                                        <th>User</th>
+                                        <th>Options</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {weatherdatas.map((weatherdata, index) => (
-                                        <tr key={weatherdata.uuid}>
+                                    {reports.map((report, index) => (
+                                        <tr key={report.uuid}>
                                             <td>{index + 1}</td>
-                                            <td>{weatherdata.location.city}</td>
-                                            <td>{moment(weatherdata.date).format("DD.MM.YYYY | HH:mm")}</td>
-                                            <td>{weatherdata.temperature} °C</td>
-                                            <td>{weatherdata.humidity} %</td>
-                                            <td>{weatherdata.precipitation} mm</td>
-                                            <td>{weatherdata.windSpeed} Km/h</td>
-                                            <td>{getWindDirection(weatherdata.windDirection)}</td>
+                                            <td>{report.title}</td>
+                                            <td>{moment(report.reportDate).format("DD.MM.YYYY | HH:mm")}</td>
+                                            <td>{report.user.imie} {report.user.nazwisko}</td>
+                                            <td>
+                                                <div className="Option">
+                                                    <Link to={`/dashboard/forecastreports/report/${report.uuid}`} className="button is-small is-info">Show More</Link>
+                                                    <button onClick={() => deleteReport(report.uuid)} className="button is-small is-danger">Delete</button>
+                                                </div>
+                                            </td>
                                         </tr>
                                     ))}
                                 </tbody>
                             </table>
-                            <button className="button is-primary" onClick={refreshData}>Refresh Data</button>
                         </div>
                     )}
                 </div>
