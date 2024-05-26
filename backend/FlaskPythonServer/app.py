6a936fed1b78a2de063701550f5e5187dead1057
Dominik Lewczyński <dominiklew200060@gmail.com>
2024-05-26 15:43:52 +0200

    Update app.py by add using .env to change port of flask app

--- backend/FlaskPythonServer/app.py
+++ backend/FlaskPythonServer/app.py
@@ -2,6 +2,10 @@ from flask import Flask
 from routes import forecast_weather
 from models import db
 from config import Config
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,5 +14,7 @@ db.init_app(app)
 
 app.route('/forecast_weather', methods=['GET'])(forecast_weather)
 
+
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    port = int(os.getenv("APP_FLASK_PORT"))
+    app.run(port=port, debug=True)
